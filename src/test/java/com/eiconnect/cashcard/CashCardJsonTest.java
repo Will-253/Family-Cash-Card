14f0e1d26c2c13f30a9c311738aef16da9177958
William Arthur <arthurw253@gmail.com>
2025-09-02 00:03:16 +0100

    Initial version

--- src/test/java/com/eiconnect/cashcard/CashCardJsonTest.java
+++ src/test/java/com/eiconnect/cashcard/CashCardJsonTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 
